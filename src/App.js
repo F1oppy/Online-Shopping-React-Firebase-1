2eb07f10f3aeca7759530af408900845b4e0ccad
F1oppy <dadheech.aditya041@gmail.com>
2022-08-08 04:09:20 +0530

    Updates and Keys

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 const promise = loadStripe(
-  "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
+  "pk_test_51LUCvgSCXXRHEkctobgrw6tdmfNDxgYqqXsJxVRBkqQV7cQB90mJGgub2TjpBJ3CLyBiinaNudrhAKpzP1y9myuC00hosnKFNL"
 );
 
 function App() {
--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'https://us-central1-challenge-ac537.cloudfunctions.net/api'
+  baseURL: 'https://asia-south1-challenge-ac537.cloudfunctions.net/api'
 });
 
 export default instance;
